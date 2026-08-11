c3d97d74adb8cf2f2293dc9190b2b07eb7f93307
ayushkumar21161 <ayush@homeasy.io>
2026-08-11 18:00:15 +0530

    Updated project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppShell } from './components/layout/AppShell';
-import ReactGA from 'react-ga4';
-
-ReactGA.initialize('G-FXHHT9RZB2');
 
 export default function App() {
   return (
